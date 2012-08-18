ef2d5ffca064fe14c3368b4697939723a5badad5
Alexandru Trican <alextrican@yahoo.com>
2012-08-17 20:18:25 -0500

    Update 1.001
    updated vars
    added cfscript block comments
    re-arranged code

--- cfsas.py
+++ cfsas.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin
 
 
-# ########################################################
+# <!---########################################################
 # ########################################################
 # ########################################################
 #   cfset - bad leading pound - <cfset[\s]*#
@@ -9,13 +9,46 @@ import sublime, sublime_plugin
 #   cfset - pound signs inside cfset w/o quotes - match # in regex: <cfset[^>""]*>
 # ########################################################
 # ########################################################		
-# ########################################################
+# ########################################################--->
 
 
 class cfstatsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		a = self.view.find_all("<\!---[\s\S]*?--->")
+		#end output message
+		returnMessage = "\nCF Stats and Standards \n General Stats:\n"
+		
 		all = self.view.find_all("[\s\S]*")
+		self.view.add_regions("AllContent", all, "source", sublime.HIDDEN)
+		g = self.view.get_regions("AllContent")
+		for allregion in g:
+			h = len(self.view.substr(allregion))
+
+		#comments
+		cf_comment_01 = self.view.find_all("<\!---[\s\S]*?--->")
+		cf_comment_02 = self.view.find_all("\/\*[\s\S]*?\*\/")
+
+				
+		self.view.add_regions("cf_comment_01", cf_comment_01, "source", sublime.HIDDEN)
+		self.view.add_regions("cf_comment_02", cf_comment_02, "source", sublime.HIDDEN)
+
+		c = 0
+		c1 = self.view.get_regions("cf_comment_01")
+		c2 = self.view.get_regions("cf_comment_02")
+
+		for region in c1:
+			c += len(self.view.substr(region))
+		for region in c2:
+			c += len(self.view.substr(region))
+		if c > 1024:			
+			returnMessage += " -Comment size is ~"+str((c/1024))+"Kb ("+str(c)+" bytes) out of a total file size of ~"+str(h/1024)+"Kb ("+str(c*100/h)+"%)\n"
+		else:
+			returnMessage += " -Comment size is ~"+str(c)+" bytes out of a total file size of ~"+str(h/1024)+"Kb ("+str(c*100/h)+"%)\n"
+
+
+		print returnMessage
+
+		
+
 		cfset_001 = self.view.find_all("<cfset[\s]*#")
 		cfset_002 = self.view.find_all("<cfset[^=\r\n]*=\s?#")
 		cfset_003 = self.view.find_all("<cfset[^>""]*>")
@@ -23,24 +56,10 @@ class cfstatsCommand(sublime_plugin.TextCommand):
 		self.view.add_regions("cfset_001",cfset_001,"source", sublime.HIDDEN)
 		self.view.add_regions("cfset_002",cfset_002,"source", sublime.HIDDEN)
 		self.view.add_regions("cfset_003",cfset_003,"source", sublime.HIDDEN)
-
-		self.view.add_regions("Highlight", a, "source", sublime.HIDDEN)
-		self.view.add_regions("AllContent", all, "source", sublime.HIDDEN)
-		b = self.view.get_regions("Highlight")
-		g = self.view.get_regions("AllContent")
-		c = 0
-		print ""
-		print "CF Stats and Standards"
-		print "---------------------------------"
-		print ">> General Stats:"
-		for allregion in g:
-			h = len(self.view.substr(allregion))
-		for region in b:
-			c += len(self.view.substr(region))
-		if c > 1024:			
-			print "Comment size is ~"+str((c/1024))+"Kb ("+str(c)+" bytes) out of a total file size of ~"+str(h/1024)+"Kb ("+str(c*100/h)+"%)"
-		else:
-			print "Comment size is ~"+str(c)+" bytes out of a total file size of ~"+str(h/1024)+"Kb ("+str(c*100/h)+"%)"		
+				
+		
+		
+				
 		print ""
 		print ">> Possible Coding Standards Violations: "
 		h = self.view.get_regions("cfset_001")
